5041197364fc8c2de8d8da058440facc10bab203
amanpal13 <amanpalk113@gmail.com>
2026-08-26 02:42:10 +0530

    Start my C++ and DSA journey

---
+++ age.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //define variable 
+    //int age = 21;
+    //cout<<"my age is :"<< age << endl ;
+    
+    //declaration
+    //int age;
+    //cout << age ;
+
+    //definition
+    //int age = 21;
+    //cout << age << endl;
+
+    //manipulation
+    //age = 101;
+    //cout << age << endl;
+
+    //int count = 5;
+    //float
+    //float share = 3.13;
+    //char
+    //char alphabhet = 'b';
+    //double
+    //double weight = 55.69886;
+    //boolean
+    //bool isMale = true;
+    //bool isChild = 1;
+    //bool isFemale = false;
+    //bool isGirl = 0;
+
+    //cout << share << endl;
+    //cout << alphabhet <<endl;
+    //cout << weight << endl;
+    //cout << isMale << endl;
+
+    int a = 24;
+    cout << sizeof(a) << endl;
+    
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "aman pal" ;
+    return 0;
+}
