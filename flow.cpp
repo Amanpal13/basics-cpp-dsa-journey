27f8d6b9eded96d980f8e550e79d1ae792bce468
amanpal13 <amanpalk113@gmail.com>
2026-08-27 01:29:33 +0530

    control flow statement

---
+++ flow.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int budget;
+    cout << "Enter your budget: " << endl;
+
+    cin >> budget;
+
+    if (budget >= 2000000) {
+        cout << "You can buy a scorpio" << endl;
+    }
+else{
+        cout << "you cannot buy a scorpio" << endl;
+        }
+    
+    
+
+    return 0;
+}
