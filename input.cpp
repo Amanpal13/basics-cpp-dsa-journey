0299a85edee2f8255813822ccaa20e28bcdea710
amanpal13 <amanpalk113@gmail.com>
2026-08-26 18:09:00 +0530

    Practice input in C++

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //variable declare
+    int marks;
+    cout << "Enter your marks :" << endl;
+
+    //give input
+    cin >> marks;
+    cout << "Your marks is : " << marks << endl;
+
+
+    return 0;
+}
