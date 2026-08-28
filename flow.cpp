81388f090d9eb49883738a07e7470202a0721bc7
amanpal13 <amanpalk113@gmail.com>
2026-08-28 23:53:47 +0530

    learning switch 1st time

--- flow.cpp
+++ flow.cpp
@@ -2,19 +2,49 @@
 using namespace std;
 
 int main(){
-    int budget;
-    cout << "Enter your budget: " << endl;
 
-    cin >> budget;
+//     int budget;
+//     cout << "Enter your budget: " << endl;
 
-    if (budget >= 2000000) {
-        cout << "You can buy a scorpio" << endl;
+//     cin >> budget;
+
+//     if (budget >= 2000000) {
+//         cout << "You can buy a scorpio" << endl;
+//     }
+//     else{
+//         cout << "you cannot buy a scorpio" << endl;
+//         }
+
+
+//     if (condition1) {
+//     // Code if condition1 is true
+// } else if (condition2) {
+//     // Code if condition1 is false, but condition2 is true
+// } else {
+//     // Fallback code if no conditions are true
+// }
+
+int height ;
+cout << "Enter your height in feet " << endl;
+cin >> height;
+
+int weight ;
+cout << "Enter your weight: " << endl;
+cin >> weight;
+
+if (height >= 5) {
+
+    if (weight >= 70) {
+        cout << "You got a good bmi" << endl;
     }
+    else{
+        cout << "u should be ashame" << endl;
+    }
+}
 else{
-        cout << "you cannot buy a scorpio" << endl;
-        }
-    
-    
+    cout << " hey short king" << endl;
+}
+
 
     return 0;
 }
---
+++ switch.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+
+    // if(grade == 'A'){
+    //     cout << "your marks will be range of 90 to 100" << endl ;
+    // }
+    // else if (grade == 'B'){
+    //      cout << "your marks will be range of 80 to 90" << endl ;
+    // }
+    // else if (grade == 'C'){
+    //     cout << "your marks will be range of 70 to 80" << endl ;
+    // }
+    // else if (grade == 'D'){
+    //     cout << "your marks will be range of 60 to 70" << endl ;
+    // }
+    // else {
+    //     cout << "your marks will be range of 0 to 60" << endl ;
+    // }
+
+    switch(grade){
+        case 'A':cout << "your marks will be range of 90 to 100" << endl ;
+        case 'a':cout << "your marks will be range of 90 to 100" << endl ;
+        break;
+        case 'B':cout << "your marks will be range of 80 to 90" << endl ;
+        case 'b':cout << "your marks will be range of 80 to 90" << endl ;
+        break;
+        case 'C':cout << "your marks will be range of 70 to 80" << endl ;
+        case 'c':   cout << "your marks will be range of 70 to 80" << endl ;
+        break;
+        case 'D':cout << "your marks will be range of 60 to 70" << endl ;
+        case 'd':cout << "your marks will be range of 60 to 70" << endl ;
+        break;
+        default:cout << "your marks will be range of 0 to 60" << endl ;
+        break;
+    }
+
+
+//     The expression used in a 
+// switch
+//  statement must be of an integral or enumerated type.
+
+// Supported data types include:
+
+// int
+// char
+// enum
+// Floating-point types such as 
+// float
+//  or 
+// double
+//  and complex data types cannot be directly used in a 
+// switch
+//  expression.
+
+    return 0;
+}
