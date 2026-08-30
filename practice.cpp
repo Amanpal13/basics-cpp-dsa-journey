fae4533b92aaa37ce931120683c3ea21c7d8d55e
amanpal13 <amanpalk113@gmail.com>
2026-08-31 00:14:34 +0530

    practicing loop

---
+++ practice.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for (int i=1 ; i<=50; i=i+1){
+    //     cout << "aman" << endl;
+    // }
+
+    // for (int i=0; i>= -10; i=i-1){
+    //     cout << i << endl;
+    // }
+
+   
+
+    for (int i = 7; i <= 70; i = i + 7) {
+        cout << i << " ";
+    }
+
+
+    for (char ch='A'; ch <= 'Z'; ch = ch + 1) {
+        cout << ch << endl;
+    }
+
+
+    for (char ch='a'; ch <= 'z' ; ch = ch +1){
+        cout << ch << endl;
+    }
+
+
+    return 0;
+}
