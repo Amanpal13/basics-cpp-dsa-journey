81ce098d4e21707712576c70b099a812f7e20abe
amanpal13 <amanpalk113@gmail.com>
2026-08-30 23:23:18 +0530

    how for loop works

---
+++ for.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 1; i <= 10; i=i+1) {
+        cout << i << endl;
+    }
+
+    //counting from 51 to 69
+    for (int i = 51; i <= 69; i=i+1) {
+        cout << i << endl;
+    }
+
+
+    return 0;
+}
