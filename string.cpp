901874fe3b48ff359c3c2b5c4ab4739662e72978
amanpal13 <amanpalk113@gmail.com>
2026-09-11 17:57:35 +0530

    function of string and how we use them

---
+++ string.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // string a = "My name is aman pal";
+    // // 11 index-> 'a'
+    // string b = "aman";
+   
+    //cout << a.find(b)<<endl;   // finding
+
+    // int ans = a.find(b);
+
+    // if(ans == string::npos) {
+    //     cout << "b string not found in a string" << endl;
+    //     //cout << string::npos << endl;
+    //  } 
+    // else {
+    //     cout << "Found" << endl;
+    // }
+
+    // string a = "aman";
+    // string b = "amanidf9uew";
+
+    // if (a.compare(b) == 0)
+    // {
+    //     cout << "strings are equal" << endl;
+    // }
+    // else
+    // {
+    //     cout << "strings are not equal" << endl;
+    // }
+
+    // string str = "statement";
+    // cout << str.substr(0, 9) << endl;   //printing from index to index which we give us as input
+
+    // string str = "goglu";
+    // string temp = "aman";
+
+    // //str.append(temp);  //we had a string with name str and we are adding temop in it
+    // str.insert(3, temp);
+    // cout << str << endl;
+
+    // cout << "length:" << str.length() << endl;
+
+    // decalre
+    //  string name;
+    //  name.push_back('a');
+    //  name.push_back('b');
+    //  name.push_back('c');
+    //  cout << name << endl;
+
+    // char arr[100];
+    // arr[0] = 'a';
+    // arr[1] = 'b';
+    // arr[2] = 'c';
+    // cout << arr<< endl;
+    //initialisation
+    string surname = "goglu";
+    cout << surname;
+
+    // //updation
+    // surname = "aman";
+
+    return 0;
+}
